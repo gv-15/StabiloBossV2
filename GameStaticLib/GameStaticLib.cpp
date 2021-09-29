bbe323d26086a477ca7465bd26a0f6a806ff2438
Gaizka <gaizkaf1ferrari@gmail.com>
2021-09-29 15:14:04 +0200

    1st commit we startinngggg

---
+++ GameStaticLib/GameStaticLib.cpp
@@ -0,0 +1,10 @@
+// GameStaticLib.cpp : Define las funciones de la biblioteca estática.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Ejemplo de una función de biblioteca
+void fnGameStaticLib()
+{
+}
