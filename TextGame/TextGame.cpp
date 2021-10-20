c0d4ec1c875b3da21a5e7c5c9236abbdbebd0187
Xabier Barguilla <xabi@orvium.io>
2021-10-20 15:48:04 +0200

    Merge with random maps

--- TextGame/TextGame.cpp
+++ TextGame/TextGame.cpp
@@ -3,23 +3,38 @@
 #include "../GameStaticLib/GameLogic.h"
 #include <thread>  
 #include "../GameStaticLib/System.h"
-#include <iostream>
 
 int main()
 {
-	World world("../game.txt");
+
+	string filename;
+
+	srand(time(NULL));
+
+	int rnd = rand() % 3 + 1;
+
+	if(rnd == 3)
+	{
+		filename = "../game3.txt";
+	}
+	else if (rnd  == 2)
+	{
+		filename = "../game2.txt";
+	}
+	else
+	{
+		filename = "../game1.txt";
+	}
+
+
+	World world(filename);
 
 	GameLogic gameLogic(&world);
 
-	cout << "Bienvenido al gran laberinto de stabillo, donde viviras una experiencia llena de color SUERTE!! "<<"\n\n"<<"Pulsa el boton espacio para empezar";
-		
-	while (!gameLogic.IsGameStarted());
-	System::Clear();
 	world.Load();
 
 	do
 	{ 
-		
 		System::SetCursorPos(0,0);
 		System::HideCursor(); 
 
