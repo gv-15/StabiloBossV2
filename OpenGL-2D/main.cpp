582d57793c34e31454ebc8d31ad31c21ffdb4c19
Gaizka Valle <gaizkaf1ferrari@gmail.com>
2021-11-24 19:21:50 +0100

    I put some walls

--- OpenGL-2D/main.cpp
+++ OpenGL-2D/main.cpp
@@ -4,6 +4,7 @@
 #include "Sprite.h"
 #include "Player.h"
 #include "World.h"
+#include "Wall.h"
 #include "Text.h"
 #include "Projectile.h"
 #include "Text.h"
@@ -35,11 +36,18 @@ int main(int argc, char** argv)
 	player1.SetName("Player1");
 	Player player2 = Player("/img/PLAYER2 VERDEAZUL SMALL", -0.9, 0, 0.084, 0.2);
 	player2.SetName("Player2");
+	Wall wall1 = Wall("/img/wall1", 0, 0, 1, 1);
+	wall1.SetName("wall1");
+	Wall wall2 = Wall("/img/wall2", -0.6, 0.4, 0.5, 0.3);
+	wall2.SetName("wall2");
+	Wall wall3 = Wall("/img/wall3", 0.7, 0.3, 0.6, 0.4);
+	wall3.SetName("wall3");
 	gameLogic.Initialize();
 	renderer.AddObject(&player1);
 	renderer.AddObject(&player2);
-
-	
+	renderer.AddObject(&wall1);
+	renderer.AddObject(&wall2);
+	renderer.AddObject(&wall3);
 
 
 	while (!gameLogic.IsGameEnded())
