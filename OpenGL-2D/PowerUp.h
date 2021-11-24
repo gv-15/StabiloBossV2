41e33b3f1e1334106dcfd9780db57e7bcfbdce03
Gaizka Valle <gaizkaf1ferrari@gmail.com>
2021-11-24 15:38:21 +0100

    Changes

---
+++ OpenGL-2D/PowerUp.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Sprite.h"
+
+class PowerUp : public Sprite
+{
+	string tipo;
+	float  Duration;
+	bool   Activated;
+
+public:
+	PowerUp(string imgagedir, double m_posx1, double m_posy1, double x1, double y2);
+	~PowerUp();
+	
+	void Activate(string s);
+	void movefaster();
+	void moveslower();
+	void youdie();
+	void teleport();
+	void ignoreObstacle();
+	string GetType();
+	void setActivated(bool b);
+
+};
+
